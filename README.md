52e090dc46c0b8f95e97b38412c6e0a8fae29b1b
Benonardo <75318636+Benonardo@users.noreply.github.com>
2023-09-18 18:49:31 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EnumReprs
+EnumReprs is a Rust library to provide traits describing the behavior of enums and their integer representations.
+Best used with [enumreprs-derive](https://github.com/Benonardo/enumreprs-derive) to derive the provided traits.
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use thiserror::Error;
+
+pub trait IntoRepr<T> {
+    fn into_repr(self) -> T;
+}
+
+pub trait FromRepr<T> {
+    fn from_repr(repr: T) -> Result<Self, FromReprError<T>>
+    where
+        Self: Sized;
+}
+
+#[derive(Error, Debug, PartialEq)]
+pub enum FromReprError<T> {
+    #[error("Invalid variant `{0}`")]
+    InvalidVariant(T)
+}
